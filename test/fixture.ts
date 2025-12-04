184a42a09b7d7567b67410b316356141d4771614
ChiHaoLu <qazwsxeric@gmail.com>
2025-12-04 15:58:12 +0800

    add basic simple account fixture

--- test/fixture.ts
+++ test/fixture.ts
@@ -1,10 +1,12 @@
 import assert from "node:assert"
 import { describe, it } from "node:test"
 
-import { isAddress } from "viem"
+import { getAddress, isAddress } from "viem"
 
 import { loadEilFixture } from "./fixture/eil.ts"
 import { loadErc4337Fixture } from "./fixture/erc4337.ts"
+import { loadSimpleMultiChainAccountFixture } from "./fixture/simpleMultiChainAccount.ts"
+import { getDeployer } from "./util/network.ts"
 
 describe("Fixture", () => {
   it("should load ERC-4337 fixture", async () => {
@@ -17,5 +19,25 @@ describe("Fixture", () => {
     for (const contract of Object.values(eilContract)) {
       assert.equal(isAddress(contract.address), true)
     }
+  })
+
+  it("should load Simple Multi Chain Account fixture", async () => {
+    const { simpleMultiChainAccount, simpleMultiChainAccountFactory } =
+      await loadSimpleMultiChainAccountFixture()
+    assert.equal(isAddress(simpleMultiChainAccount.address), true)
+    assert.equal(isAddress(simpleMultiChainAccountFactory.address), true)
+
+    const deployer = await getDeployer()
+    assert.equal(
+      simpleMultiChainAccount.address,
+      await simpleMultiChainAccountFactory.read.getAddress([
+        deployer.account.address,
+        0n
+      ])
+    )
+    assert.equal(
+      getAddress(deployer.account.address),
+      await simpleMultiChainAccount.read.owner()
+    )
   })
 })
