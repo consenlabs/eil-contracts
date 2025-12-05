f95ebbc85068175a4bd85b23cc8d201925ac5e1f
pilagod <pilagooood@gmail.com>
2025-12-05 10:16:35 +0800

    keep single quote convention

--- test/fixture.ts
+++ test/fixture.ts
@@ -1,18 +1,18 @@
-import assert from "node:assert"
-import { describe, it } from "node:test"
+import assert from 'node:assert'
+import { describe, it } from 'node:test'
 
-import { isAddress } from "viem"
+import { isAddress } from 'viem'
 
-import { loadEilFixture } from "./fixture/eil.ts"
-import { loadErc4337Fixture } from "./fixture/erc4337.ts"
+import { loadEilFixture } from './fixture/eil.ts'
+import { loadErc4337Fixture } from './fixture/erc4337.ts'
 
-describe("Fixture", () => {
-  it("should load ERC-4337 fixture", async () => {
+describe('Fixture', () => {
+  it('should load ERC-4337 fixture', async () => {
     const { entryPoint } = await loadErc4337Fixture()
     assert.equal(isAddress(entryPoint.address), true)
   })
 
-  it("should load EIL fixture", async () => {
+  it('should load EIL fixture', async () => {
     const eilContract = await loadEilFixture()
     for (const contract of Object.values(eilContract)) {
       assert.equal(isAddress(contract.address), true)
